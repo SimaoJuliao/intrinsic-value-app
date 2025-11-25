dfd142da0215c3156cc59390931b8478561aeb9e
Simao <simao.juliao.projects@gmail.com>
2025-11-25 21:21:28 +0000

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Vite + React</h1>
+      <div className="card">
+        <button
+          type="submit"
+          onClick={() => setCount((count) => count + 1)}
+          className=""
+        >
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
