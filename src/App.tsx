f5830ba544948ad7aad6348dc95353c51ae3ca57
Simao <simao.juliao.projects@gmail.com>
2025-11-27 21:24:16 +0000

    Added first configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
-import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Typography } from "./components";
+import { Field, FieldGroup, FieldLabel, FieldSet } from "./components/ui/field";
+import { Input } from "./components/ui/input";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Vite + React</h1>
-      <div className="card">
-        <button
-          type="submit"
-          onClick={() => setCount((count) => count + 1)}
-          className=""
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+    <div className="bg-background flex flex-col items-center gap-5 ">
+      <Typography type="h1">Intrinsic Value Calculator</Typography>
+
+      <div className="w-full max-w-md p-5">
+        <form>
+          <FieldGroup>
+            <FieldSet>
+              <Field>
+                <FieldLabel htmlFor="ticker-name">Insert the ticker</FieldLabel>
+                <Input required id="ticker-name" placeholder="AMD" />
+              </Field>
+            </FieldSet>
+
+            <Field orientation="responsive">
+              <Button type="submit">Submit</Button>
+            </Field>
+          </FieldGroup>
+        </form>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
